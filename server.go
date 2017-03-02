06abffb60b635be0bd432389b7173bf0f89058bc
Christian Moen <cmoen11@gmail.com>
2017-03-02 19:28:36 +0100

    jobbet litt med the web page

---
+++ server.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/go-martini/martini"
+	"github.com/martini-contrib/render"
+	"net/http"
+	"fmt"
+)
+
+func main() {
+	m := martini.Classic()
+	// render html templates from templates directory
+	m.Use(render.Renderer())
+
+	m.Get("/", func(r render.Render) {
+		r.HTML(200, "hello", "usernames...")
+	})
+
+	m.Post("/submitUsername", func(req *http.Request){
+		fmt.Println(req.PostForm)
+	})
+
+	m.Run()
+}
