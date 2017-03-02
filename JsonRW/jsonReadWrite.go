4eb50f763faf234ad67b96fdddbc8c42f383042a
Martin Nenseth <martin.nenseth@gmail.com>
2017-03-02 20:49:18 +0100

    ..

--- JsonRW/jsonReadWrite.go
+++ JsonRW/jsonReadWrite.go
@@ -16,7 +16,7 @@ type jsonItems struct {
  * Creates a new instance to the json file.
  */
 func WriteInstance(name string, ip string)  {
-	//appends the parameters to the struct.
+	//assign the parameters to the struct.
 	row := jsonItems{Name : name, Ip: ip}
 
 	b, err := json.Marshal(row)
@@ -27,5 +27,26 @@ func WriteInstance(name string, ip string)  {
 	ioutil.WriteFile("output1.json", b, 0644)
 	fmt.Println(b)
 }
+/*
+ * Decodes the json file and prints its content.
+ */
+func ReadEntireJson ()  {
+	jsonFile, err := ioutil.ReadFile("output1.json")
+	if err != nil{
+		fmt.Print(err)
+	}
+	var items jsonItems // Variable to store the content of the file.
+	err = json.Unmarshal(jsonFile, &items)
+	if err != nil{
+		fmt.Println(err)
+	}
+	fmt.Println(items)
+}
+
+
+
+
+
+
 
 
--- server.go
+++ server.go
@@ -1,13 +1,16 @@
 package main
 
-import (
+import (	/**
 	"github.com/go-martini/martini"
 	"github.com/martini-contrib/render"
 	"net/http"
 	"fmt"
+	*/
+	"./JsonRW"
 )
 
 func main() {
+	/*
 	m := martini.Classic()
 	// render html templates from templates directory
 	m.Use(render.Renderer())
@@ -19,8 +22,10 @@ func main() {
 	m.Post("/submitUsername", func(req *http.Request){
 		fmt.Println(req.PostForm)
 	})
-
 	m.Run()
+*/
 
+	//JsonRW.WriteInstance("erik", "1992.2.")
+	JsonRW.ReadEntireJson()
 
 }
