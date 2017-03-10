5bdf30a78fbe93869276dcfb54f25c98dd4dec2e
Christian Moen <cmoen11@gmail.com>
2017-03-10 17:40:29 +0100

    added request call at show username... That will force it to show latest usernames.

--- server.go
+++ server.go
@@ -50,6 +50,9 @@ func main() {
 		Render all the usernames we have collected so far.
 	 */
 	m.Get("/members", func(r render.Render) {
+		// To force check that the node got the latest file, send update request
+		SendUpdateRequests()
+
 		r.HTML(200, "header", "")
 		r.HTML(200, "header-text", "Members we have collected so far")
 		// for each member in our json file
@@ -149,12 +152,17 @@ func main() {
 
 	m.RunOnAddr(":5050")
 	m.Run()
+
+
 }
 
 /*
 	For sending out update requests to other hosts in the network.
  */
 func SendUpdateRequests() {
+
+	println("Sending update request to other nodes..")
+
 	// collect list of servers, based on the json file with usernames..
 	servers := JsonRW.GetAllIPs()
 
@@ -172,6 +180,7 @@ func SendUpdateRequests() {
 	form.Add("token", "someTokenToPreventUnauthoriseUpdateRequest")
 	for _, ip := range servers {
 		if ip != host_ip {
+			println("Sending update request for " + ip)
 			url := "http://" + ip +":8080" + "/api/runUpdate"
 			req, err := http.NewRequest("POST", url, strings.NewReader(form.Encode()))
 
